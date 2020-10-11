13cc0cb7804364f201c501f895a604f368eaceab
chiragdoctor <chirag.doctor@gmail.com>
2020-10-11 15:51:21 +0530

    Cmomple Input component render tests for success=false

--- src/Input.js
+++ src/Input.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux'
 
 export class Input extends Component {
     render() {
+        const contents = this.props.success ? null : (
+            <form className="fonm-inline">
+                <input type="text" data-test="input-box" classNamew="mb-2 mx-sm-3" placeholder="enter guess" />
+                <button type="submit" data-test="submit-button"  className="btn btn-primary mb-2">Submit</button>
+            </form>
+        )
         return (
-            <div>
-                
+            <div data-test="component-input">
+                {contents}
             </div>
         )
     }
 }
 
-const mapStateToProps = (state) => ({
-    
+const mapStateToProps = ({success}) => ({
+    success,
 });
 
 const mapDispatchToProps = {
