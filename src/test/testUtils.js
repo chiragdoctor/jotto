d61b8c0d8bcb79a6f19294f653d4ca4c0e8a1b20
chiragdoctor <chirag.doctor@gmail.com>
2020-10-11 15:32:54 +0530

    Setup Input Component and storeFactory test utils

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,4 +1,12 @@
 import checkPropTypes  from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../reducers';
+
+
+export const storeFactory = (initialState) => {
+  return createStore(rootReducer, initialState);
+}
 
 export const findByTestAttr = (wrapper, val) =>
   wrapper.find(`[data-test="${val}"]`);
